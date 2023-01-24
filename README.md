4aeed88ef07b986a268836a4e04a62748830d890
Guilherme Morum Santos <g@numih.com>
2023-01-24 11:31:13 -0300

    pizza

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# moz·za·rel·la
+
+*/ˌmɒtsəˈrɛlə/*
+
+  **Mozzarella**  sis a southern Italian  **cheese**  traditionally made from Italian buffalo's milk by the pasta filata method.  [[ref]](https://en.wikipedia.org/wiki/Mozzarella)
+
+**Pizza**  is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with  **cheese**,  and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven.  [[ref]](https://en.wikipedia.org/wiki/Pizza)
+
+**just the best**
+
+# Intro
+
+Here lies the source for [mozzarella.pizza](https://mozzarella.pizza), made for and by **pizza** lovers.
+All love is aprecitated.
+
+*stay mozzarella,*
+*stay pizza*
---
+++ src/components/Main.svelte
@@ -0,0 +1,19 @@
+<script></script>
+
+<main class="flex items-center h-screen justify-center w-full">
+    <div class="max-w-2xl p-4">
+        <h1 class="sm:text-8xl text-6xl w-full text-center mb-2">moz·za·rel·la</h1>
+        <h3 class="sm:text-2xl text-xl font-light italic w-full text-center mb-12 text-gray-400">/ˌmɒtsəˈrɛlə/</h3>
+        <p class="w-full text-justify mb-2">
+            <b>Mozzarella</b> sis a southern Italian <b>cheese</b> traditionally made from Italian buffalo's milk by the pasta filata method. 
+            <a class="text-xs text-blue-800" target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Mozzarella">[ref]</a>
+        </p>
+        <p class="w-full text-justify">
+            <b>Pizza</b> is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with 
+            <b>cheese</b>, <strike>and often various other ingredients</strike>, which is then baked at a high temperature, traditionally in a wood-fired 
+            oven.
+            <a class="text-xs text-blue-800" target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Pizza">[ref]</a>
+        </p>
+        <h3 class="text-sm font-bold w-full text-center mt-12">just the best</h3>
+    </div>
+</main>
